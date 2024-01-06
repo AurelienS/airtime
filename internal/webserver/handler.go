c96d1775cdc52efda132c81c0f76e76fc0f72d64
AurelienS <aurelien.sberro@touch-sell.com>
2024-01-06 01:21:56 +0100

    Add layout

--- internal/webserver/handler.go
+++ internal/webserver/handler.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 
 	"github.com/AurelienS/cigare/internal/storage/sqlc"
-	"github.com/AurelienS/cigare/web/template"
+	"github.com/AurelienS/cigare/web/template/page"
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 )
@@ -17,7 +17,7 @@ type Handler struct {
 func (h *Handler) GetIndex(c echo.Context) error {
 	flights, _ := h.Queries.GetFlights(context.Background())
 	test := strconv.Itoa(len(flights))
-	return render(c, template.Index(test))
+	return render(c, page.Index(test))
 }
 
 func render(c echo.Context, component templ.Component) error {
