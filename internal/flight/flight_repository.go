8d3a8ec25a5dbb295837f3557967032bd97ef295
AurelienS <aurelien.sberro@touch-sell.com>
2024-01-10 03:43:41 +0100

    Add log

--- internal/flight/flight_repository.go
+++ internal/flight/flight_repository.go
@@ -2,6 +2,7 @@ package flight
 
 import (
 	"context"
+	"time"
 
 	flightstats "github.com/AurelienS/cigare/internal/flight_statistic"
 	"github.com/AurelienS/cigare/internal/storage"
@@ -27,6 +28,8 @@ func (r FlightRepository) InsertFlight(
 	user storage.User,
 
 ) error {
+	util.Info().Str("user", user.Email).Msg("Inserting flight")
+
 	insertFlightParams := storage.InsertFlightParams{
 		Date:            flight.Date,
 		TakeoffLocation: flight.TakeoffLocation,
@@ -38,17 +41,23 @@ func (r FlightRepository) InsertFlight(
 	transaction := func() error {
 		flightStatId, err := r.insertFlightStats(ctx, flightStats)
 		if err != nil {
+			util.Error().Err(err).Str("user", user.Email).Msg("Failed to insert flight statistics")
 			return err
 		}
 		insertFlightParams.FlightStatisticsID = flightStatId
 
 		_, err = r.queries.InsertFlight(ctx, insertFlightParams)
+		if err != nil {
+			util.Error().Err(err).Str("user", user.Email).Msg("Failed to insert flight")
+		}
 		return err
 	}
 	return r.tm.ExecuteTransaction(ctx, transaction)
 }
 
 func (r FlightRepository) GetFlights(ctx context.Context, user storage.User) ([]storage.Flight, error) {
+	util.Info().Str("user", user.Email).Msg("Getting flights")
+
 	flights, err := r.queries.GetFlights(ctx, user.ID)
 	if err != nil {
 		util.Error().Err(err).Str("user", user.Email).Msg("Failed to get flights")
@@ -57,6 +66,8 @@ func (r FlightRepository) GetFlights(ctx context.Context, user storage.User) ([]
 }
 
 func (f FlightRepository) insertFlightStats(ctx context.Context, flightStat flightstats.FlightStatistics) (int32, error) {
+	util.Info().Msg("Inserting flight statistics")
+
 	param := storage.InsertFlightStatsParams{
 		TotalThermicTime:  storage.DurationToPGInterval(flightStat.TotalThermicTime),
 		TotalFlightTime:   storage.DurationToPGInterval(flightStat.TotalFlightTime),
@@ -70,8 +81,21 @@ func (f FlightRepository) insertFlightStats(ctx context.Context, flightStat flig
 	}
 	id, err := f.queries.InsertFlightStats(ctx, param)
 	if err != nil {
+		util.Error().Err(err).Msg("Failed to insert flight statistics")
 		return 0, err
 	}
 
 	return id, nil
+}
+
+func (f FlightRepository) GetTotalFlightTime(ctx context.Context, userId int) (time.Duration, error) {
+	util.Info().Int("user_id", userId).Msg("Getting total flight time")
+
+	flightTimeMicroseconds, err := f.queries.GetTotalFlightTime(ctx, int32(userId))
+	if err != nil {
+		util.Error().Err(err).Int("user_id", userId).Msg("Failed to get total flight time")
+		return 0, err
+	}
+
+	return time.Duration(flightTimeMicroseconds) * time.Microsecond, nil
 }
