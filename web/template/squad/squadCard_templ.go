bbb63c48df99a02e32b1004ca4ff05e0c95463e6
AurelienS <aurelien.sberro@touch-sell.com>
2024-01-14 00:26:29 +0100

    remove unused

--- web/template/squad/squadCard_templ.go
+++ web/template/squad/squadCard_templ.go
@@ -10,8 +10,6 @@ import "context"
 import "io"
 import "bytes"
 
-import "github.com/AurelienS/cigare/web/template/component"
-
 import "strconv"
 import "github.com/AurelienS/cigare/internal/model"
 
@@ -39,7 +37,7 @@ func SquadCard(squad model.Squad) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(memberCount(squad))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/template/squad/squadCard.templ`, Line: 12, Col: 26}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/template/squad/squadCard.templ`, Line: 10, Col: 26}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
@@ -70,7 +68,7 @@ func SquadCard(squad model.Squad) templ.Component {
 		var templ_7745c5c3_Var5 string
 		templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(memberCount(squad))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/template/squad/squadCard.templ`, Line: 14, Col: 26}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/template/squad/squadCard.templ`, Line: 12, Col: 26}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 		if templ_7745c5c3_Err != nil {
