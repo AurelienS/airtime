b6315b58013bc449b5f5615150c2b468eb5f2c51
AurelienS <aurelien.sberro@touch-sell.com>
2024-01-18 06:50:52 +0100

    ensure client redirect to login page if needed

--- web/middleware/auth_middleware.go
+++ web/middleware/auth_middleware.go
@@ -15,6 +15,13 @@ func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 		user, err := session.GetUserOrErrorFromContext(c)
 		if err != nil {
 			util.Warn().Msg("user not logged in. Will be redirected soonA")
+
+			if c.Request().Header.Get("HX-Request") != "" {
+				// Instruct HTMX to redirect the full page
+				c.Response().Header().Set("HX-Redirect", "/login")
+				return nil
+			}
+			// Standard full-page redirect
 			return c.Redirect(http.StatusFound, "/login")
 		}
 
