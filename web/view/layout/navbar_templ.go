589f8c6df9fa94e8d3993fb22afb4cf49d69c2d7
AurelienS <aurelien.sberro@touch-sell.com>
2024-02-10 01:46:25 +0100

    test

--- web/view/layout/navbar_templ.go
+++ web/view/layout/navbar_templ.go
@@ -29,7 +29,7 @@ func Navbar(currentUser viewmodel.UserView, showNavigation bool) templ.Component
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Var2 := `Airtime`
+		templ_7745c5c3_Var2 := `Airtime2`
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var2)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
